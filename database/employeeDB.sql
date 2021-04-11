d05d5235594871e2e14b103b20448ed0cf3da861
Josh A <jmalm79@gmail.com>
2021-04-11 09:58:22 -0500

    moved seeds into its own .sql file, moving forward will use employeeDB.sql, then employeeSeed.sql

---
+++ database/employeeDB.sql
@@ -0,0 +1,48 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+
+    dept_id INTEGER(10) NOT NULL,
+
+    dept_name VARCHAR(30),
+
+    PRIMARY KEY (dept_id)
+
+);
+
+CREATE TABLE employee_role (
+
+    role_id INTEGER(10) NOT NULL,
+
+    title VARCHAR(40) NOT NULL,
+
+    salary INTEGER(10),
+
+    dept_id INTEGER(10),
+
+    is_manager BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY (role_id),
+
+    FOREIGN KEY (dept_id) REFERENCES department(dept_id)
+);
+
+CREATE TABLE employee (
+
+    employee_id INTEGER(10) AUTO_INCREMENT NOT NULL,
+
+    first_name VARCHAR(30) NOT NULL,
+
+    last_name VARCHAR(30) NOT NULL,
+
+    role_id INTEGER(10),
+
+    PRIMARY KEY (employee_id),
+
+    FOREIGN KEY (role_id) REFERENCES employee_role(role_id)
+
+);
--- database/employeeSeed.sql
+++ database/employeeSeed.sql
@@ -1,47 +1,5 @@
-DROP DATABASE IF EXISTS employeeDB;
-
-CREATE DATABASE employeeDB;
 
 USE employeeDB;
-
-CREATE TABLE department (
-
-    dept_id INTEGER(10) NOT NULL,
-
-    dept_name VARCHAR(30),
-
-    PRIMARY KEY (dept_id)
-
-);
-
-CREATE TABLE employee_role (
-
-    role_id INTEGER(10) NOT NULL,
-
-    title VARCHAR(40) NOT NULL,
-
-    salary INTEGER(10),
-
-    dept_id INTEGER(10),
-
-    is_manager BOOLEAN DEFAULT FALSE,
-
-    PRIMARY KEY (role_id)
-);
-
-CREATE TABLE employee (
-
-    employee_id INTEGER(10),
-
-    first_name VARCHAR(30) NOT NULL,
-
-    last_name VARCHAR(30) NOT NULL,
-
-    role_id VARCHAR(30),
-
-    manager_id VARCHAR(10)
-
-);
 
 INSERT INTO department (dept_id, dept_name)
 VALUES 
@@ -68,16 +26,27 @@ INSERT INTO employee_role (role_id, title, salary, dept_id)
 VALUES   
     (12, "Sales Rep", 75000, 10),
     (13, "Marketing Associate", 70000, 10),
+    (22, "Engineer", 125000, 20),
+    (23, "Developer", 120000, 20),
     (32, "Sr. Purchasor", 65000, 30),
     (33, "Jr. Purchasor", 40000, 30),
     (42, "Press Operator", 40000, 40),
     (43, "Parts Runner", 30000, 40),
     (44, "Material Handler", 35000, 40),
-    (22, "Engineer", 125000, 20),
-    (23, "Developer", 120000, 20),
     (52, "Shipping Associate", 32500, 50),
     (53, "Receiving Associate", 32500, 50),
     (92, "HR Generalist", 65000, 90),
     (102, "Maintenance Technician", 55000, 100),
     (103, "Custodian", 37500, 100);
     
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ("Aaron", "Paul",11),
+    ("Mindy", "Kaling",13),
+    ("Oscar","Isaac",31),
+    ("Kate","Hudson",103),
+    ("Jason","Momoa",43),
+    ("John","Krasinski",31),
+    ("Kevin","Hart",92),
+    ("Cooper","Barnes",102),
+    ("Jennifer","Hewitt",44);
